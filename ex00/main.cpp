ddefe8ea6b4f96287cfded447598958dc2914967
ggjulio <juligonz@student.42.fr>
2021-02-06 02:12:51 +0100

    ex00 refacto

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,16 +6,18 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 18:21:30 by juligonz          #+#    #+#             */
-/*   Updated: 2021/01/31 14:37:10 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/02/06 01:34:57 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 #include <iostream>
+#include <sstream>
 #include <string>
 
 int main(){
 	FragTrap fragTP("Fr4g-TP-v4");
+	std::stringstream ss;
 
 	fragTP.meleeAttack("BAD_TRAVELER");
 	fragTP.rangedAttack("BAD_TRAVELER");
@@ -28,14 +30,19 @@ int main(){
 	std::cout << std::endl;
 	std::cout << "Actual energy:" << fragTP.getEnergyPoints() << std::endl;
 	fragTP.setEnergyPoints(-200);
-	fragTP.print(std::string("<energy>(-200)")+" updated energy:"+ std::to_string(fragTP.getEnergyPoints()));
+	ss << "<energy>(-200) updated energy:" << fragTP.getEnergyPoints();
+	fragTP.print(ss.str());
+	ss.str("");
 
 	fragTP.setEnergyPoints(200);
-	fragTP.print(std::string("<energy>(200)")+" updated energy:"+ std::to_string(fragTP.getEnergyPoints()));
+	ss << "<energy>(200) updated energy:" << fragTP.getEnergyPoints();
+	fragTP.print(ss.str());
+	ss.str("");
 
 	std::cout << std::endl << std::endl;
 	fragTP.setEnergyPoints(-2);
-	fragTP.print(std::string("<energy>(-2)")+" updated energy:"+ std::to_string(fragTP.getEnergyPoints()));
+	ss << "<energy>(-2) updated energy:" << fragTP.getEnergyPoints();
+	fragTP.print(ss.str());
 
 	fragTP.vaulthunter_dot_exe("BAD_TRAVELER");
 	fragTP.vaulthunter_dot_exe("BAD_TRAVELER");
